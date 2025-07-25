46993ab343abb8ce0695363e5aeda564ee9b43fc
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-07-25 12:21:13 +0200

    additional webpack config for dockerode

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   watchOptions: {
     pollIntervalMs: 1000,
+  },
+  webpack: (config, {isServer}) => {
+    if(!isServer) {
+      config.externals.push({
+        'dockerode': 'commonjs dockerode',
+        'ssh2': 'commonjs ssh2',
+      });
+    }
   }
 };
 
