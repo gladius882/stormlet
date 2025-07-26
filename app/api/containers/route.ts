63f2fa4ae5f5fcffe230721530e86dace241f35a
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-26 11:48:01 +0200

    list containers api route

--- app/api/containers/route.ts
+++ app/api/containers/route.ts
@@ -4,7 +4,10 @@ import Docker from "dockerode"
 import { NextResponse } from "next/server"
 
 export async function GET() {
-  const docker = new Docker({ socketPath: "/var/run/docker.sock" })
+  const docker = new Docker({
+    host: "192.168.0.24",
+    port: "2375"
+  })
   const containers = await docker.listContainers()
   return NextResponse.json(containers)
 }
