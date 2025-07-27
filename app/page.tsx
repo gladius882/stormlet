8e563305429f91fee2d58401835332ade9f7fb64
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-27 09:59:25 +0200

    simple sign in form

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,74 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { Button, TextField } from "@mui/material";
+import { signIn, useSession } from "next-auth/react";
+import { useState } from "react";
 
 export default function Home() {
 
-  const [containers, setContainers] = useState([])
+	const [login, setLogin] = useState("");
+	const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    fetch("/api/containers")
-      .then(res => res.json())
-      .then(setContainers)
-  }, [])
+	const { data: session, status } = useSession();
 
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Elo 3</h1>
+	const handleSignIn = async () => {
 
-      <div>
-        {JSON.stringify(containers[0])}
-      </div>
-    </div>
-  );
+		const res = await signIn("credentials", {
+			redirect: false,
+			login,
+			password
+		});
+
+		if(res?.ok) {
+			console.log('Signed in');
+		} else {
+			console.log("Error");
+		}
+	}
+
+	return (
+		<div className="h-[100vh] flex justify-center items-center">
+			<div className="w-1/3 shadow-md flex flex-col bg-transparent">
+				<div className="text-center bg-gray-300 text-emerald-500 text-xl">
+					stormlet
+				</div>
+
+				<div className="flex gap-0">
+					<TextField
+						id="outlined-controlled"
+						label="Login"
+						value={login}
+						onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+							setLogin(event.target.value);
+						}}
+					/>
+				</div>
+
+				<div>
+					<TextField
+						id="outlined-controlled"
+						type="password"
+						label="Password"
+						value={password}
+						onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+							setPassword(event.target.value);
+						}}
+					/>
+				</div>
+
+				<div>
+					<Button onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+						handleSignIn();
+					}} className="w-1/2" variant="contained" color="success">Sing in</Button>
+				</div>
+
+				<div>
+					<p>{status}</p>
+					{JSON.stringify(session)}
+				</div>
+			</div>
+
+			
+		</div>
+	);
 }
