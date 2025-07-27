a54ef6d17c4e9068026d2519a0b55a10dc0c0add
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-27 12:45:25 +0200

    list environments in env selector component

--- modules/environment/components/EnvironmentSelector.tsx
+++ modules/environment/components/EnvironmentSelector.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
+import { listEnvironments } from "../lib/environment"
+import { EnvironmentType } from "../types";
+
 export default function () {
+
+    const [envs, setEnvs] = useState<EnvironmentType[]>([]);
+
+    useEffect(() => {
+        fetch("/api/environments")
+            .then((res) => res.json())
+            .then((data) => {
+                setEnvs(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [])
+
     return (
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center min-w-[240px]">
             <div>ENV:</div>
-            <select className="bg-emerald-300 h-full text-gray-900 rounded-sm p-2">
-                <option>NO ENVIRONMENT</option>
-                <option>local</option>
-                <option>proxmox</option>
-                <option>orangepi</option>
+            <select className="bg-emerald-300 h-full w-full text-gray-900 rounded-sm p-2">
+                {envs.length === 0 && <option value="0">NO ENVIRONMENT</option>}
+                {envs.map(e => {
+                    return (
+                        <option value={e.id}>{e.name}</option>
+                    )
+                })}
             </select>
         </div>
     )
