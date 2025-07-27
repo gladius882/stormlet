7ce9f839ae491aedaccf169f4510682a4923666d
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-27 14:06:17 +0200

    no environment message component

--- app/(core)/dashboard/page.tsx
+++ app/(core)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@mui/material";
 import Link from "next/link";
 
 import { DockerWarningIcon } from "@/icons";
+import NoEnvironmentsMessage from "@/modules/theme/components/NoEnvironmentsMessage";
 
 export default async function Dashboard() {
 
@@ -11,25 +12,7 @@ export default async function Dashboard() {
 
     return (
         <div className="w-full h-full">
-            {envs.length === 0 && (
-                <div className="w-full h-full flex justify-center items-center">
-                    <div className="text-center flex flex-col gap-5">
-                        <div className="flex justify-center">
-                            <DockerWarningIcon width={200} height={140} className="fill-emerald-600"/>
-                        </div>
-
-                        <div className="text-xl">
-                            There is no environemnt defined
-                        </div>
-
-                        <Link href="/environments/new">
-                            <Button size="large" variant="outlined" color="success">
-                                Add your first environment
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            )}
+            {envs.length === 0 && <NoEnvironmentsMessage />}
 
         </div>
     )
