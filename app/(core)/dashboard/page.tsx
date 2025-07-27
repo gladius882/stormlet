17ad0d81bd01943422f6688da399e79cbb82d0ce
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-27 12:19:23 +0200

    sidebar navigation component

--- app/(core)/dashboard/page.tsx
+++ app/(core)/dashboard/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 
-import logo from "@/images/logo-transparent.png"
-import Image from "next/image"
-import Link from "next/link"
-import DirectionsBoatFilledOutlinedIcon from '@mui/icons-material/DirectionsBoatFilledOutlined';
 import Popper from "@mui/material/Popper";
 import Button from "@mui/material/Button";
 import { useRef, useState } from "react";
 import { ClickAwayListener, Grow, MenuItem, MenuList, Paper } from "@mui/material";
 import { signOut } from "next-auth/react";
+import SidebarNavigation from "@/modules/theme/components/SidebarNavigation";
 
 export default function Dashboard() {
 
@@ -38,37 +35,13 @@ export default function Dashboard() {
 
     return (
         <div className="flex min-w-[100vh]">
-            <nav className="w-[280px] flex flex-col bg-emerald-600 px-5 h-full gap-3">
-                <div className="self-center mb-10">
-                    <Image width={180} src={logo} alt="" />
-
-                </div>
-
-                <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
-                    Dashboard
-                </Link>
-                <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
-                    Containers
-                </Link>
-                <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
-                    Images
-                </Link>
-                <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
-                    Volumes
-                </Link>
-                <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
-                    Networks
-                </Link>
-                <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
-                    Dashboard
-                </Link>
-            </nav>
+            <SidebarNavigation />
 
             <div className="flex flex-col w-full">
 
                 <div className="sticky bg-emerald-800 text-gray-50 flex items-center justify-between px-5">
 
-                    <div className="flex items-center">
+                    <div className="flex gap-2 items-center">
                         <div>ENV:</div>
                         <select className="bg-emerald-300 h-full text-gray-900 rounded-sm p-2">
                             <option>NO ENVIRONMENT</option>
---
+++ modules/theme/components/SidebarNavigation.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "@/images/logo-transparent.png"
+
+export default function () {
+    return (
+        <nav className="w-[280px] flex flex-col bg-emerald-600 px-5 h-full gap-3">
+            <div className="self-center mb-10">
+                <Image width={180} src={logo} alt="" />
+
+            </div>
+
+            <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
+                Dashboard
+            </Link>
+            <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
+                Containers
+            </Link>
+            <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
+                Images
+            </Link>
+            <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
+                Volumes
+            </Link>
+            <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
+                Networks
+            </Link>
+            <Link href="/" className="text-gray-900 bg-emerald-300 rounded-full p-2 font-bold text-center">
+                Dashboard
+            </Link>
+        </nav>
+    )
+}
