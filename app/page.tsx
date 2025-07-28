1d96ea01afd895b92407bacbfc9d5e45eeea1938
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-07-28 10:26:25 +0200

    styles for login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
-import { Button, TextField } from "@mui/material";
+import { Alert, Button, TextField } from "@mui/material";
 import { signIn, useSession } from "next-auth/react";
-import { useState } from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import logo from "@/images/logo-transparent.png";
 
 export default function Home() {
 
+	const [isError, setIsError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [login, setLogin] = useState("");
 	const [password, setPassword] = useState("");
 
@@ -13,62 +17,68 @@ export default function Home() {
 
 	const handleSignIn = async () => {
 
+		setIsLoading(true);
+
 		const res = await signIn("credentials", {
 			redirect: false,
 			login,
 			password
 		});
 
-		if(res?.ok) {
+		if (res?.ok) {
 			console.log('Signed in');
 		} else {
-			console.log("Error");
+			setIsError(true);
+			setIsLoading(false);
 		}
 	}
 
 	return (
-		<div className="h-[100vh] flex justify-center items-center">
-			<div className="w-1/3 shadow-md flex flex-col bg-transparent">
-				<div className="text-center bg-gray-300 text-emerald-500 text-xl">
-					stormlet
-				</div>
+		<div className="h-[100vh] flex flex-col items-center py-10">
+			<Image width={200} src={logo} alt="sdad" className="mb-10" />
 
-				<div className="flex gap-0">
-					<TextField
-						id="outlined-controlled"
-						label="Login"
-						value={login}
-						onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-							setLogin(event.target.value);
-						}}
-					/>
-				</div>
+			<div className="w-1/4 shadow-md flex flex-col p-5 items-center gap-5">
 
-				<div>
-					<TextField
-						id="outlined-controlled"
-						type="password"
-						label="Password"
-						value={password}
-						onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-							setPassword(event.target.value);
-						}}
-					/>
-				</div>
+				<TextField
+					id="outlined-controlled"
+					label="Login"
+					value={login}
+					onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+						setLogin(event.target.value);
+					}}
+					className="w-full"
+				/>
 
-				<div>
-					<Button onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
-						handleSignIn();
-					}} className="w-1/2" variant="contained" color="success">Sing in</Button>
-				</div>
+				<TextField
+					id="outlined-controlled"
+					type="password"
+					label="Password"
+					value={password}
+					onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+						setPassword(event.target.value);
+					}}
+					className="w-full"
+				/>
 
-				<div>
+
+				<Button onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+					handleSignIn();
+				}} className="w-full" variant="contained" color="success">
+					Sing in
+					{isLoading && "..."}
+				</Button>
+
+				<div className="w-full">
 					<p>{status}</p>
 					{JSON.stringify(session)}
 				</div>
-			</div>
 
-			
+				{isError && (
+					<Alert severity="error" className="w-full" >
+						Invalid credentials
+					</Alert>
+				)}
+			</div>
 		</div>
 	);
 }
