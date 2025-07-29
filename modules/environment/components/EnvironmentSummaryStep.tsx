dae149ecfc969a1e84722ca61f77204bc93102d0
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-29 19:57:04 +0200

    in environment add summary display only field related to selected connection type

--- modules/environment/components/EnvironmentSummaryStep.tsx
+++ modules/environment/components/EnvironmentSummaryStep.tsx
@@ -16,9 +16,10 @@ export default function EnvironmentSummaryStep(props: EnvironmentSummaryStepProp
             <Typography sx={{ mt: 2, mb: 1 }}>
                 All steps completed - you&apos;re finished
             </Typography>
-            
+
             <div className="w-full flex justify-center">
                 <div className="grid grid-cols-2 w-[80%] gap-5">
+
 
                     <div>Type</div>
                     <TextField defaultValue={props.data.type} disabled={true} />
@@ -26,26 +27,34 @@ export default function EnvironmentSummaryStep(props: EnvironmentSummaryStepProp
                     <div>Name</div>
                     <TextField defaultValue={props.data.name} disabled={true} />
 
-                    <div>Host</div>
-                    <TextField defaultValue={props.data.host} disabled={true} />
+                    {props.data.type !== "socket" && (
+                        <>
+                            <div>Host</div>
+                            <TextField defaultValue={props.data.host} disabled={true} />
 
-                    <div>Port</div>
-                    <TextField defaultValue={props.data.port} disabled={true} />
+                            <div>Port</div>
+                            <TextField defaultValue={props.data.port} disabled={true} />
 
-                    <div>Protocol</div>
-                    <TextField defaultValue={props.data.protocol} disabled={true} />
+                            {props.data.type === "secure-api" && (
+                                <>
+                                    <div>Protocol</div>
+                                    <TextField defaultValue={props.data.protocol} disabled={true} />
 
-                    <div>Version</div>
-                    <TextField defaultValue={props.data.version} disabled={true} />
+                                    <div>Version</div>
+                                    <TextField defaultValue={props.data.version} disabled={true} />
 
-                    <div>CA</div>
-                    <TextField defaultValue={props.data.ca} disabled={true} />
+                                    <div>CA</div>
+                                    <TextField defaultValue={props.data.ca} disabled={true} />
 
-                    <div>CERT</div>
-                    <TextField defaultValue={props.data.cert} disabled={true} />
+                                    <div>CERT</div>
+                                    <TextField defaultValue={props.data.cert} disabled={true} />
 
-                    <div>KEY</div>
-                    <TextField defaultValue={props.data.key} disabled={true} />
+                                    <div>KEY</div>
+                                    <TextField defaultValue={props.data.key} disabled={true} />
+                                </>
+                            )}
+                        </>
+                    )}
 
                 </div>
             </div>
