1da1652b7474d9a168ab53a04b002f7448b28bf9
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-07-30 18:16:26 +0200

    list containers from environment api route

--- app/api/containers/route.ts
+++ app/api/containers/route.ts
@@ -1,13 +1,43 @@
 "use server"
 
+import { PrismaClient } from "@/app/generated/prisma";
 import Docker from "dockerode"
-import { NextResponse } from "next/server"
+import { NextApiRequest } from "next"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET() {
-  const docker = new Docker({
-    host: "192.168.0.24",
-    port: "2375"
-  })
-  const containers = await docker.listContainers()
-  return NextResponse.json(containers)
+export async function GET(req: NextRequest) {
+
+	const params = req.nextUrl.searchParams;
+
+	if (params.has('env') === false) {
+		return NextResponse.json({
+			error: true,
+			message: "There is no `env` param query"
+		}, { status: 400 })
+	}
+
+	const env_id = params.get('env') as string;
+
+	const prisma = new PrismaClient();
+
+	const env = await prisma.environment.findFirst({
+		where: {
+			id: parseInt(env_id)
+		}
+	})
+
+	if (env === null) {
+		return NextResponse.json({
+			error: true,
+			message: `Can't load environment with ID ${env_id}`,
+		}, { status: 400 })
+	}
+
+	const docker = new Docker({
+		host: env.host as string,
+		port: env.port as number
+	})
+
+	const containers = await docker.listContainers()
+	return NextResponse.json(containers)
 }
