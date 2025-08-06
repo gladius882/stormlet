2b72ff7a9f0be75f3df412351006882e45bdc219
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-08-06 06:44:08 +0200

    environment lib with bearer authorization

--- modules/environment/lib/environment.ts
+++ modules/environment/lib/environment.ts
@@ -1,5 +1,12 @@
+import { signToken } from "@/lib/jwt";
+
 export const listEnvironments = async() => {
+
+    const headers = new Headers();
+    headers.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImxvZ2luIjoiYWRtaW4iLCJpYXQiOjE3NTM4OTYyNDQsImV4cCI6MTc1Mzg5OTg0NH0.9U0nLVgoSnZEWk5JRQLvaTnKMOJgdIwt-AauU4CWSvI")
+
     const res = await fetch("http://localhost:3000/api/environments", {
+        headers
     })
 
     return res.json();
