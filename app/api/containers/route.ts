f9ff5caeaf1a1d2b7a38f4ea2a512629a8c83770
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-08-06 06:44:28 +0200

    containers api route auth testing

--- app/api/containers/route.ts
+++ app/api/containers/route.ts
@@ -1,11 +1,23 @@
 "use server"
 
 import { PrismaClient } from "@/app/generated/prisma";
+import { requireBearerToken } from "@/lib/auth-api";
+import { signToken } from "@/lib/jwt";
 import Docker from "dockerode"
 import { NextApiRequest } from "next"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
+
+	// const authResult = requireBearerToken(req);
+
+	// console.log(authResult);
+
+	// if(authResult.success === false) {
+	// 	return NextResponse.json({
+	// 		message: "Invalid or missing credentials"
+	// 	}, { status: 401 })
+	// }
 
 	const params = req.nextUrl.searchParams;
 
